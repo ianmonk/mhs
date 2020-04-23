9456e5de4b576908c60bc46438e9cddecf357945
Ian Monk <ianmonk@Ians-MacBook-Pro.local>
2020-04-23 11:41:35 -0400

    updates

--- js/custom.js
+++ js/custom.js
@@ -11,6 +11,7 @@
 	
 	$(window).scroll(function(evt){
 		check_if_in_view();
+		scroll_actions();
 	});
 	
 	function check_if_in_view() {
@@ -34,6 +35,58 @@
 	    }
 	  });
 	  
+	}
+	
+	function scroll_actions(){
+		
+		var sec1 = $(".mhs-intro").offset();
+		var sec2 = $(".mhs-gallery").offset();
+		var sec3 = $(".mhs-spaces").offset();
+		var sec4 = $(".mhs-pricing").offset();
+		var sec5 = $(".mhs-contact").offset();
+		var sec6 = $(".mhs-faq").offset();
+		
+		var vpos = $(document).scrollTop();
+	
+		var sec1_trigger = sec1.top-100;
+		var sec2_trigger = sec2.top-100;
+		var sec3_trigger = sec3.top-100;
+		var sec4_trigger = sec4.top-100;
+		var sec5_trigger = sec4.top-100;
+		var sec6_trigger = sec4.top-100;
+	
+		if(vpos < sec1.top){
+			$('#menu-homepage-navigation li.menu-item').addClass("active");
+		}
+		if((vpos >= sec1_trigger) && (vpos < sec2_trigger)) {
+			$('#menu-homepage-navigation li.menu-item').removeClass("active");
+			$('#menu-homepage-navigation li.menu-item-23').addClass("active");
+		}
+		else if ((vpos >= sec2_trigger) && (vpos < sec3_trigger)) {
+			$('#menu-homepage-navigation li.menu-item').removeClass("active");
+			$('#menu-homepage-navigation li.menu-item-25').addClass("active");
+		}
+		else if ((vpos >= sec3_trigger) && (vpos < sec4_trigger)) {
+			$('#menu-homepage-navigation li.menu-item').removeClass("active");
+			$('#menu-homepage-navigation li.menu-item-26').addClass("active");
+		}
+		else if ((vpos >= sec4_trigger) && (vpos < sec5_trigger)) {
+			$('#menu-homepage-navigation li.menu-item').removeClass("active");
+			$('#menu-homepage-navigation li.menu-item-24').addClass("active");
+		}
+		else if ((vpos >= sec5_trigger) && (vpos < sec6_trigger)) {
+			$('#menu-homepage-navigation li.menu-item').removeClass("active");
+			$('#menu-homepage-navigation li.menu-item-27').addClass("active");
+		}
+	
+		if((vpos >= sec2.top) && (vpos < sec3.top)){
+			$(".mhs-primary-nav").addClass("mhs-dark");
+			$(".mhs-logo").addClass("mhs-dark");
+		} else {
+			$(".mhs-primary-nav").removeClass("mhs-dark");
+			$(".mhs-logo").removeClass("mhs-dark");
+		}
+		
 	}
 	
 	$(window).load(function(){
